1f95fe91bd85e98e841dc1204f1f66d3075f2d0f
Sam Mason <sam.jbmason@gmail.com>
2014-01-11 15:50:20 +0000

    Get first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :lists
+  has_many :gifts
+
+  def first_name
+    name.split(' ')[0] if name
+  end
 end
