0d032ac41c25512a306c321b1319f62f6c5c8863
Sam Mason <sam.jbmason@gmail.com>
2014-01-11 15:50:30 +0000

    Schema been updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110112312) do
+ActiveRecord::Schema.define(version: 20140111124703) do
 
   create_table "gifts", force: true do |t|
     t.string   "name"
-    t.decimal  "price"
+    t.decimal  "price",      precision: 10, scale: 2
     t.string   "link"
-    t.boolean  "been_bought"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "list_id"
