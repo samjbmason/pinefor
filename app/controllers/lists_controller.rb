948a791845fe2bf321d28d7742c4b208d401fd2d
Sam Mason <sam.jbmason@gmail.com>
2014-01-11 15:51:40 +0000

    Save list data

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,12 +6,23 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new
+    @list = List.new(list_params)
 
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      redirect_to lists_url, alert: 'List was not created, try again.'
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @gift = Gift.new
+    @gifts = @list.gifts.all
   end
 
   private
   def list_params
-    params.require(:list).permit(:name, :date, :url)
+    params.require(:list).permit(:name, :natural_due_date)
   end
 end
