7887e2bdf1b178f1dbca9aad4f0459afaebff216
Sam Mason <sam.jbmason@gmail.com>
2014-01-12 20:28:35 +0000

    Split out login form to shared partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # All of this code allows me to have the account forms as partials in toher views
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
