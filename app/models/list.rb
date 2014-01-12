34919ffe190483e35011306734bba0ecc152dab9
Sam Mason <sam.jbmason@gmail.com>
2014-01-12 20:29:42 +0000

    Increase secure string length and comment code

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,16 +6,19 @@ class List < ActiveRecord::Base
 
   before_save :create_unique_url
 
+  # This creates a unique string then checks if it exists if it does it runs loop again until unique is found
   def create_unique_url
     begin
-    self.url = SecureRandom.urlsafe_base64(8)
+    self.url = SecureRandom.urlsafe_base64(10)
     end while self.class.exists?(url: url)
   end
 
+  # Getter for easy way to get nice date
   def natural_due_date
     due_date.strftime('%d/%m/%Y') if due_date
   end
 
+  # Setter to convert nice date to db save-able date format
   def natural_due_date=(date)
     self.due_date = Chronic.parse(date) if date.present?
   end
