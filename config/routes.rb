afadaa2640fb2bf9d7a63c62c769bf2f817c015e
Sam Mason <sam.jbmason@gmail.com>
2014-01-30 16:38:53 +0000

    Create custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Pinefor::Application.routes.draw do
   get 'public/:public_hash', to: 'lists#public', as: :public_list
   put 'public/:public_hash/gifts/:id', to: 'gifts#public_update', as: :public_gift_update
 
+  match ':status', to: 'errors#show', constraints: {status: /\d{3}/ }, via: :all
 end
