649f52786e50ecf4f26a4a5e1cf0402ecbb24032
Sam Mason <sam.jbmason@gmail.com>
2014-01-30 19:10:10 +0000

    Update routes make a bit more proper ordering

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Pinefor::Application.routes.draw do
 
+  get 'public/:public_hash', to: 'lists#public', as: :public_list
+
+  put 'public/:public_hash/gifts/:id', to: 'gifts#public_update', as: :public_gift_update
+
   authenticated :user do
     root 'lists#index', as: :authenticated_root
     get '/lists', to: redirect('/')
   end
 
-  root 'pages#welcome'
-
-  devise_for :users, :path => ''
-
   resources :lists do
     resources :gifts
   end
 
-  get 'public/:public_hash', to: 'lists#public', as: :public_list
-  put 'public/:public_hash/gifts/:id', to: 'gifts#public_update', as: :public_gift_update
+  devise_for :users, :path => ''
+
+  root 'pages#welcome'
 
   match ':status', to: 'errors#show', constraints: {status: /\d{3}/ }, via: :all
 end
