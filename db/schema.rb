0388c94eda683d23a4e7b83d2510a3f1dfeaa2e8
Sam Mason <sam.jbmason@gmail.com>
2014-01-31 16:11:26 +0000

    Create Counter cache migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130165029) do
+ActiveRecord::Schema.define(version: 20140131155121) do
 
   create_table "gifts", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140130165029) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "gifts_count"
   end
 
   create_table "users", force: true do |t|
