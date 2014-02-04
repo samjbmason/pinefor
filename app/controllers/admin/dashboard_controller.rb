e9c2e994dccfcf8526e4008a99c1405665479b3e
Sam Mason <sam.jbmason@gmail.com>
2014-02-04 16:56:42 +0000

    Create logic for chart, bit lengthy

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,6 +8,15 @@ class Admin::DashboardController < ApplicationController
 
     @user_count = User.count
     @latest_user = User.last
+
+    # Creates empty array that matches number of marks on chart
+    value_array = Array.new(8).fill(0)
+    # Fetches 8 metric records and order them old -> new
+    @all_user_counts = Metric.user_count.order(:created_at).limit(8).where("created_at < ?", Time.now)
+    # Loops through results and pushes value onto array and removes first in array.
+    @all_user_counts.map { |m| value_array.push(m.value).shift }
+    # Now can use array
+    @values = value_array
   end
 
   private
