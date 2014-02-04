25490edd13c104dc9d56f2d757f503d6a2cff80f
Sam Mason <sam.jbmason@gmail.com>
2014-02-04 16:56:59 +0000

    Create scope for specific metric more future proofing

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,5 +1,6 @@
 class Metric < ActiveRecord::Base
   validates :name, presence: true, inclusion: { in: %w(user_count) }
-
   validates :value, presence: true, numericality: { only_integer: true }
+
+  scope :user_count, -> { where(name: "user_count") }
 end
